82f3f33433fbbf48198f5fa00ce48998fbc4d60a
lemontree2000 <gonelove.guo@gmail.com>
2020-01-16 17:19:16 +0800

    chore: 自动化构建index pen 部署

--- build/prepublish.gulpfile.js
+++ build/prepublish.gulpfile.js
@@ -1,18 +1,19 @@
 const fse = require("fs-extra");
 const htmlparser2 = require("htmlparser2");
 const Path = require("path");
-const puppeteer = require("puppeteer-core")
+const puppeteer = require("puppeteer");
+
 /**
  * 1、解析html 生成数据
  * 2、生成截图
  * 3、生成html， 插入index.html
  */
-
+let currentCommentData;
 
 const parser = new htmlparser2.Parser(
   {
     oncomment(data) {
-      console.log(data)
+      currentCommentData = commentToData(data);
     },
     onend(data) {
       console.log("end");
@@ -20,25 +21,70 @@ const parser = new htmlparser2.Parser(
   },
   { decodeEntities: true }
 );
+// 注释转数据
+function commentToData(comment = "") {
+  return comment
+    .split("\n")
+    .filter(line => /date|lang/g.test(line))
+    .map(info => {
+      const d = info.trim().split(":");
+      return { [d[0]]: d[1].trim() };
+    })
+    .reduce((v, c, i) => (v = { ...v, ...c }), {});
+}
 
-function prePublish(cb) {
+// 解析Html
+function parseHtml(filePath) {
+  const fileBuffer = fse.readFileSync(filePath);
+  parser.write(fileBuffer.toString());
+}
+
+// 生成截图
+async function generateSrceenShot(browser, filename, file) {
+  const page = await browser.newPage();
+  await page.goto(`http://localhost:8888/page/${file}`);
+  const dom = await page.$("[screenshot]");
+  await dom.screenshot({ path: Path.resolve(`src/image/${filename}.png`) });
+}
+
+// 填充index模板
+function replaceTemplate(penSet) {
+  let penHtml = "";
+  for (const pen of penSet) {
+    penHtml += `
+      <li>
+        <section>
+            <a href="./page/${pen.filename}.html">
+                <img  src="./image/${pen.filename}.png" alt="">
+            </a>
+        </section>
+        <div>
+            <span class="name">${pen.filename}</span>
+            <span class="date">${pen.date}</span>
+        </div>
+        <p>${pen.lang}</p>
+      </li>`;
+  }
+  const templateStr = fse.readFileSync(Path.resolve('src/temp.html')).toString();
+  const pens = templateStr.replace('{{li}}', penHtml);
+  fse.writeFileSync(Path.resolve('src/index.html'), pens);
+}
+
+async function prePublish(cb) {
   const files = fse.readdirSync(Path.resolve("src/page"));
-  files.map(file => {
+  const browser = await puppeteer.launch();
+  const penSet = new Set();
+
+  for (const file of files) {
     const filePath = Path.resolve(`src/page/${file}`);
-    const fileBuffer = fse.readFileSync(filePath);
-    parser.write(fileBuffer.toString());
-  });
+    const filename = file.split(".")[0];
+    parseHtml(filePath);
+    await generateSrceenShot(browser, filename, file);
+    penSet.add({ filename, ...currentCommentData });
+  }
 
-
-  (async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto('https://baidu.com');
-    await page.screenshot({path: 'example.png'});
-  
-    await browser.close();
-  })();
-
+  replaceTemplate(penSet)
+  await browser.close();
   cb();
 }
 
